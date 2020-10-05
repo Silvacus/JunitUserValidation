7f1669c804ab68b77624351225170ae061187702
Silvacus <49201311+Silvacus@users.noreply.github.com>
2020-10-05 23:08:46 +0530

    [Kevin]added-UserJunittest.java

---
+++ test/java/com/capgemini/userJuinttest/UserRegistrationTest.java
@@ -0,0 +1,18 @@
+package com.capgemini.userJuinttest;
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class UserRegistrationTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
