4735e0cf8fe8c7c587fbd8c15fd0531f258e9d72
Silvacus <49201311+Silvacus@users.noreply.github.com>
2020-10-05 23:27:17 +0530

    [Kevin]refactor:added ability to validate password with minimum 8 chars

--- main/java/com/capgemini/userJuinttest/UserRegistration.java
+++ main/java/com/capgemini/userJuinttest/UserRegistration.java
@@ -1,6 +1,5 @@
 package com.capgemini.userJuinttest;
 
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.util.Scanner;
@@ -23,6 +22,10 @@ public class UserRegistration {
 	
 	private boolean validateMobileNo(String mobileNo) {
 		return mobileNo.matches("^[0-9]{2}[ ][0-9]{10}$");
+	}
+	
+	private boolean validatePassword(String password) {
+		return password.matches("^.{8,}$");
 	}
 
 	public static void main( String[] args ) {
@@ -55,5 +58,12 @@ public class UserRegistration {
 			LOG.info("Valid Mobile No");
 		else
 			LOG.info("Invalid Mobile No");
+
+		LOG.info("Password: ");
+		String password = sc.nextLine();
+		if(userRegistration.validatePassword(password))
+			LOG.info("Valid Password");
+		else
+			LOG.info("Invalid Password");
 	} 
 }
